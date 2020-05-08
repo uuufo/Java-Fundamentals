b39f7ca517dd6566c394507f268603717a5c4041
Jared Larsen <jaredjoel@me.com>
2020-05-08 10:02:15 -0500

    fixed Exercise_05

--- src/labs_examples/datatypes_operators/labs/Exercise_05.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_05.java
@@ -19,7 +19,6 @@ class LogicalOperators {
         // write your code below
 
         boolean a = true;
-<<<<<<< HEAD
         boolean b = false;
 
         if (a) {
@@ -53,42 +52,7 @@ class LogicalOperators {
         if (!b) {
             System.out.println("b is false");
         }
-=======
-        boolean b = true;
 
-        if (a) {
-            System.out.println("a is true");
-        } else {
-            System.out.println("a is false");
-        }
-        if (b) {
-            System.out.println("b is true");
-        } else {
-            System.out.println("b is false");
-        }
-        if (a & b) {
-            System.out.println("a and b are true");
-        }
-        if (a && b) {
-            System.out.println("a and b are true");
-        }
-        if (a | b) {
-            System.out.println("a or b are true");
-        }
-        if (a || b) {
-            System.out.println("a or b are true");
-        }
-        if (a ^ b) {
-            System.out.println("either a or b are true, but not both");
-        }
-        if (!a) {
-            System.out.println("a is false");
-        }
-        if (!b) {
-            System.out.println("b is false");
-        }
-
->>>>>>> d3821eeb3eba1d4ba85f902ca82cc35208c27dc3
 
     }
 
