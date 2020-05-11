32376f9191e87360ea5245e786a22da8f9fc2d7e
Jared Larsen <jaredjoel@me.com>
2020-05-11 10:07:50 -0500

    completed Exercise_02 & Exercise_03

--- src/labs_examples/conditions_loops/labs/Exercise_02.java
+++ src/labs_examples/conditions_loops/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 2: Days of the week
  *
@@ -13,9 +15,52 @@ public class Exercise_02 {
     public static void main(String[] args) {
 
         // 1) create scanner (don't forget to import Scanner!)
+
+        Scanner prompt = new Scanner(System.in);
+
         // 2) prompt user
+
+        System.out.print("Enter a number between 1 and 10: ");
+
         // 3) assign input to variable as int
+
+        int i = prompt.nextInt();
+
         // 4) write completed code here
 
+        while (true) {
+
+            if (i < 11 && i > 0) {
+                if (i == 1) {
+                    System.out.println("Monday!");
+                    break;
+                } else if (i == 2) {
+                    System.out.println("Tuesday!");
+                    break;
+                } else if (i == 3) {
+                    System.out.println("Wednesday!");
+                    break;
+                } else if (i == 4) {
+                    System.out.println("Thursday!");
+                    break;
+                } else if (i == 5) {
+                    System.out.println("Friday!");
+                    break;
+                } else if (i == 6) {
+                    System.out.println("Saturday!");
+                    break;
+                } else if (i == 7) {
+                    System.out.println("Sunday!");
+                    break;
+                } else if (i > 7 && i < 11) {
+                    System.out.println("Other!");
+                    break;
+                }
+            } else {
+                System.out.print("Enter a number between 1 and 10! : ");
+                i = prompt.nextInt();
+                continue;
+            }
+        }
     }
 }
