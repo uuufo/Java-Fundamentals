6f72ddf4a8f14c83a30638bf9f8dff54b2443eab
Jared Larsen <jaredjoel@me.com>
2020-05-14 11:26:15 -0500

    completed arrays Exercise_01

--- src/labs_examples/arrays/labs/Exercise_01.java
+++ src/labs_examples/arrays/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.arrays.labs;
 
+import java.util.Scanner;
+
 /**
  * Arrays calculator
  *
@@ -11,5 +13,31 @@ package labs_examples.arrays.labs;
  */
 
 public class Exercise_01 {
+
+    public static void main(String[] args) {
+
+        int s = 0;
+        int a = 0;
+        int nums[] = new int[10];
+
+        Scanner prompt = new Scanner(System.in);
+        System.out.println("I'm going to prompt you to enter 10 numbers.");
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print("Please enter number " + (i + 1) + ": ");
+            nums[i] = prompt.nextInt();
+        }
+
+        for (int x : nums) {
+            s += x;
+            a = s / nums.length;
+        }
+
+        System.out.println("The sum of all 10 numbers is: " + s);
+        System.out.println("The average of all 10 numbers is: " + a);
+
+
+    }
+
 
 }
