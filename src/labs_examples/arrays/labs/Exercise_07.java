baae421ff7c3881fbe7106bb9830040b2fc9bdee
Jared Larsen <jaredjoel@me.com>
2020-05-17 08:45:45 -0600

    completed array Exercise_07

--- src/labs_examples/arrays/labs/Exercise_07.java
+++ src/labs_examples/arrays/labs/Exercise_07.java
@@ -1,5 +1,9 @@
 package labs_examples.arrays.labs;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 /**
  *  ArrayLists
  *
@@ -10,4 +14,38 @@ package labs_examples.arrays.labs;
  *
  */
 public class Exercise_07 {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<String>();
+        Scanner prompt = new Scanner(System.in);
+
+        System.out.print("Please enter a name: ");
+        String firstName = prompt.nextLine();
+        names.add(firstName + " Joe");
+        names.add(firstName + " Smith");
+        names.add(firstName + " Larsen");
+        names.add(firstName + " Vion");
+        names.add(firstName + " Rutherford");
+
+        for (String fullName : names) {
+            System.out.println(fullName);
+        }
+
+        names.remove(0);
+
+        if (names.size() == 4) {
+            System.out.println("A few more!");
+            names.add(firstName + " McIntyre");
+            names.add(firstName + " Morgan");
+            names.add(firstName + " Gilberg");
+        }
+
+        Collections.sort(names);
+
+        for (String fullName : names) {
+            System.out.println(fullName);
+        }
+    }
+
 }
