48a14fa2d40a5c2e989098bf9fe066462e557c90
Jared Larsen <jaredjoel@me.com>
2020-06-07 09:54:45 -0700

    more labs

--- src/labs_examples/objects_classes_methods/labs/methods/MethodTraining.java
+++ src/labs_examples/objects_classes_methods/labs/methods/MethodTraining.java
@@ -3,16 +3,24 @@ package labs_examples.objects_classes_methods.labs.methods;
 public class MethodTraining {
 
     public static void main(String[] args) {
+
         int[] test = {1, 4, 568, 400, 8, 15, 567};
         double[] test2 = {3.45, 1.2, 40.5, 32.9};
-        System.out.println(getLarge(test));
-        System.out.println(getLarge(test2));
+        System.out.println(getLargeArr(test));
+        System.out.println(getLargeArr(test2));
 
-        System.out.println(getConst("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+        System.out.println(getLarge(3769, 645, 6345, 874));
+
+        int[] hiLo = new int[2];
+        hiLo = getHiLo(test);
+        System.out.println("Low: " + hiLo[0] + ", High: " + hiLo[1]);
+
+        System.out.println(getConst("test"));
+        System.out.println(getConst2("test"));
 
         printBart();
 
-        System.out.println(isPrime(2));
+        System.out.println(isPrime(11));
     }
 
     static boolean isPrime(int x) {
@@ -27,7 +35,27 @@ public class MethodTraining {
         return true;
     }
 
-    static int getLarge(int[] arr) {
+    static int[] getHiLo(int[] arr) {
+        int h = 0;
+        int l = 0;
+        int[] m = new int[2];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > h) {
+                h = arr[i];
+            }
+        }
+        l = h;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < l) {
+                l = arr[i];
+            }
+        }
+        m[0] = l;
+        m[1] = h;
+        return m;
+    }
+
+    static int getLargeArr(int[] arr) {
         int m = 0;
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] > m) {
@@ -37,7 +65,7 @@ public class MethodTraining {
         return m;
     }
 
-    static double getLarge(double[] arr) {
+    static double getLargeArr(double[] arr) {
         double m = 0.0;
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] > m) {
@@ -47,13 +75,40 @@ public class MethodTraining {
         return m;
     }
 
-    static String getConst(String s) {
+    static int getLarge(int a, int b, int c, int d) {
+        int m = a;
+        if (b > m) {
+            m = b;
+        }
+        if (c > m) {
+            m = c;
+        }
+        if (d > m) {
+            m = d;
+        }
+        return m;
+    }
+
+    static int getConst(String s) {
         String c = "bcdfghjklmnpqrstvwxyzBCDGHJKLMNPQRSTFWXYZ";
-        String result = "";
+        int result = 0;
         for (int i = 0; i < s.length(); i++) {
             for (int x = 0; x < c.length(); x++) {
                 if (s.charAt(i) == c.charAt(x)) {
-                    result += s.charAt(i);
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+
+    static int getConst2(String s) {
+        String v = "aeiouAEIOU";
+        int result = s.length();
+        for (int i = 0; i < s.length(); i++) {
+            for (int x = 0; x < v.length(); x++) {
+                if (s.charAt(i) == v.charAt(x)) {
+                    result--;
                 }
             }
         }
