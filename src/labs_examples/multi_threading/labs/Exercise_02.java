7114f6e7306c9a38a681988bee8ec850b459faa7
Jared Larsen <jaredjoel@me.com>
2020-09-01 10:53:48 -0700

    multi_threading labs

--- src/labs_examples/multi_threading/labs/Exercise_02.java
+++ src/labs_examples/multi_threading/labs/Exercise_02.java
@@ -5,3 +5,33 @@ package labs_examples.multi_threading.labs;
  *
  *      Create an application that creates a Thread using the Thread class
  */
+
+class Exercise_02 {
+    public static void main(String[] args) {
+
+        ThreadTest thread1 = new ThreadTest("Thread 1");
+        ThreadTest thread2 = new ThreadTest("Thread 2");
+
+    }
+}
+
+class ThreadTest extends Thread {
+
+    String name;
+    public ThreadTest(String name) {
+        super(name);
+        start();
+    }
+    @Override
+    public void run() {
+        System.out.println(getName() + " starting.");
+        try {
+            for (int i = 0; i < 10; i++) {
+                Thread.sleep(100);
+                System.out.println(getName() + " count " + i);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(getName() + " interrupted!");
+        }
+    }
+}
--- src/labs_examples/multi_threading/labs/Exercise_03.java
+++ src/labs_examples/multi_threading/labs/Exercise_03.java
@@ -5,3 +5,37 @@ package labs_examples.multi_threading.labs;
  *
  *      In one of the previous exercises, demonstrate changing the priority of a thread
  */
+
+class Exercise_03 {
+    public static void main(String[] args) {
+
+        ThreadTest2 thread1 = new ThreadTest2("Thread 1", 10);
+        ThreadTest2 thread2 = new ThreadTest2("Thread 2", 8);
+        ThreadTest2 thread3 = new ThreadTest2("Thread 3", 2);
+        ThreadTest2 thread4 = new ThreadTest2("Thread 4", 1);
+
+    }
+}
+
+class ThreadTest2 extends Thread {
+
+    String name;
+    public ThreadTest2(String name, int priority) {
+        super(name);
+        setPriority(priority);
+        start();
+    }
+    @Override
+    public void run() {
+        System.out.println(getName() + " starting.");
+        try {
+            for (int i = 0; i < 10; i++) {
+                Thread.sleep(100);
+                System.out.println(getName() + " count " + i);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(getName() + " interrupted!");
+        }
+        System.out.println(getName() + " finished.");
+    }
+}
