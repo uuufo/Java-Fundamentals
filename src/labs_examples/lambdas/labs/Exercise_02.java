d4bdf66f73ab5ba48e79172177c0a187a3c9fe2f
Jared Larsen <jaredjoel@me.com>
2020-09-30 11:55:13 -0700

    more lambda lab work

--- src/labs_examples/lambdas/labs/Exercise_02.java
+++ src/labs_examples/lambdas/labs/Exercise_02.java
@@ -1,5 +1,8 @@
 package labs_examples.lambdas.labs;
 
+import java.time.LocalTime;
+import java.util.function.*;
+
 /**
  * Lambdas Exercise 2:
  *
@@ -7,3 +10,41 @@ package labs_examples.lambdas.labs;
  *      10 of them! You can do it! You'll know so much about them when you're done!
  *
  */
+
+class Exercise_02 {
+    public static void main(String[] args) {
+
+        Supplier<LocalTime> time = LocalTime::now;
+        System.out.println("Current Time: " + time.get());
+
+        Predicate<Integer> p = i -> (i > 10);
+        System.out.println("Is 20 greater than 10? " + p.test(20));
+
+        BiPredicate<Integer, Integer> p2 = (x, y) -> x < y;
+        System.out.println("Is 20 less than 10? " + p2.test(20, 10));
+
+        BinaryOperator<Integer> b1 = (x, y) -> x * y;
+        System.out.println(b1.apply(5, 7));
+
+        Function<Integer, String> f1 = String::valueOf;
+        String test2 = f1.apply(500);
+        System.out.println(test2);
+
+        BiFunction<Integer, Integer, String> f2 = (x, y) -> String.valueOf(x + y);
+        String test3 = f2.apply(500, 500);
+        System.out.println(test3);
+
+        Consumer<String> c1 = System.out::println;
+        c1.accept("Hello World!");
+
+        BiConsumer<String, String> c2 = (s1, s2) -> System.out.println(s1 + " " + s2);
+        c2.accept("Hello", "World!");
+
+        ToDoubleBiFunction<Integer, Integer> multiply = (x, y) -> (x * y);
+        System.out.println(multiply.applyAsDouble(5, 7));
+
+        UnaryOperator<Integer> half = x -> (x / 2);
+        System.out.println(half.apply(500));
+
+    }
+}
